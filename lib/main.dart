7c1ad636c6a118e3737bc5261bebbe001f0cfe9e
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-08-10 09:30:14 -0500

    llistview builder example

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:recetasappg9/pages/form_page.dart';
 import 'package:recetasappg9/pages/home_page.dart';
+import 'package:recetasappg9/pages/list_view_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ListViewPage(),
     debugShowCheckedModeBanner: false,
   ));
 }
---
+++ lib/pages/list_view_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ListViewPage extends StatelessWidget {
+  List<String> names = ["Pedro", "Ana", "Juana", "Robert", "LUCIA"];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Text("Hola esta es mi lista de containers"),
+            Expanded(
+              child: ListView.builder(
+                itemCount: names.length,
+                itemBuilder: (BuildContext context, int index) {
+                  print(index);
+                  return Container(
+                    // width: 200,
+                    height: 200,
+                    margin: EdgeInsets.all(8),
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: Text(
+                      names[index],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Container(
+              // width: 200,
+              height: 100,
+              margin: EdgeInsets.all(8),
+              color: Colors.black,
+              alignment: Alignment.center,
+              child: Text(
+                "FIN",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+
+        // ListView(
+        //   children: [
+        //     Text("Hola esta es mi lista de containers"),
+        //     ...List.generate(
+        //       10,
+        //       (index) => Container(
+        //         // width: 200,
+        //         height: 200,
+        //         margin: EdgeInsets.all(8),
+        //         color: Colors.red,
+        //       ),
+        //     ),
+        //     Container(
+        //       // width: 200,
+        //       height: 200,
+        //       margin: EdgeInsets.all(8),
+        //       color: Colors.black,
+        //       alignment: Alignment.center,
+        //       child: Text(
+        //         "FIN",
+        //         style: TextStyle(color: Colors.white),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+      ),
+    );
+  }
+}
